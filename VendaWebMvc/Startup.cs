2a1a17dc2a9c665c650de2a76172cf7aafd5d161
Giovanni-Sena <giovanni.sena@outlook.com>
2021-07-11 14:14:57 -0300

    Conexão MySql e primeira migration

--- VendaWebMvc/Startup.cs
+++ VendaWebMvc/Startup.cs
@@ -37,7 +37,7 @@ namespace VendaWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<VendaWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("VendaWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("VendaWebMvcContext"), builder => builder.MigrationsAssembly("VendaWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
