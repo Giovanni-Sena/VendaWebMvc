304131d33769b1b336ea7190a666dc8bfb456d97
Giovanni-Sena <giovanni.sena@outlook.com>
2021-07-25 15:01:00 -0300

    Criado tratativas de erros personalizados

--- VendaWebMvc/Controllers/VendedoresController.cs
+++ VendaWebMvc/Controllers/VendedoresController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,14 +25,14 @@ namespace VendaWebMvc.Controllers
         public IActionResult Index()
         {
             var lista = _vendedorService.BuscarTodos();
-            
+
             return View(lista);
         }
 
         public IActionResult Criar()
         {
             var departamentos = _departamentoService.BuscarTodos();
-            var viewModel = new VendedorFormViewModel {Departamentos = departamentos };
+            var viewModel = new VendedorFormViewModel { Departamentos = departamentos };
             return View(viewModel); // Incluir o viewModel para retorna a lista de departamento
         }
         [HttpPost] // Incluindo para utilizar o metedo Post
@@ -42,26 +43,6 @@ namespace VendaWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
         public IActionResult Deletar(int? id)
-        {
-            if(id == null)
-            {
-                return NotFound();
-            }
-            var obj = _vendedorService.EncontrarPorId(id.Value);
-            if(obj == null)
-            {
-                return NotFound();
-            }
-            return View(obj);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Deletar(int id)
-        {
-            _vendedorService.Remover(id);
-            return RedirectToAction(nameof(Index));
-        }
-        public IActionResult Detalhes (int? id)
         {
             if (id == null)
             {
@@ -74,17 +55,37 @@ namespace VendaWebMvc.Controllers
             }
             return View(obj);
         }
-
-        public IActionResult Editar(int? id)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Deletar(int id)
         {
-            if(id == null)
+            _vendedorService.Remover(id);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Detalhes(int? id)
+        {
+            if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id está null!" });
             }
             var obj = _vendedorService.EncontrarPorId(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id não encontrado!" });
+            }
+            return View(obj);
+        }
+
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id está null!"});
+            }
+            var obj = _vendedorService.EncontrarPorId(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não encontrado!" });
             }
             List<Departamento> departamentos = _departamentoService.BuscarTodos();
             VendedorFormViewModel viewModel = new VendedorFormViewModel { Vendedor = obj, Departamentos = departamentos };
@@ -95,23 +96,33 @@ namespace VendaWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Editar(int id, Vendedor vendedor)
         {
-            if(id != vendedor.Id)
+            if (id != vendedor.Id)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id não corresponde!" });
             }
             try
             {
                 _vendedorService.Atualizar(vendedor);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (NotFoundException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+            catch (DbConcurrencyException e)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
+        }
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
+
