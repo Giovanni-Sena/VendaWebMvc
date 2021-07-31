fa92ac49383a6d06d9350ea959f38e3f28cd66d5
Giovanni-Sena <giovanni.sena@outlook.com>
2021-07-31 15:54:33 -0300

    Criado a página de pesquisa de vendas

---
+++ VendaWebMvc/Controllers/RegistroDeVendasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendaWebMvc.Controllers
+{
+    public class RegistroDeVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaPorGrupo()
+        {
+            return View();
+        }
+    }
+}
