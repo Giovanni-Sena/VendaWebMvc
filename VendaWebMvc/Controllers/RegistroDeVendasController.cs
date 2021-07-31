68d5f5ed0b790dd762bcecb7b7bd259adfe99901
Giovanni-Sena <giovanni.sena@outlook.com>
2021-07-31 17:23:17 -0300

    Ajustado a visualização de data no filtro na busca simples

--- VendaWebMvc/Controllers/RegistroDeVendasController.cs
+++ VendaWebMvc/Controllers/RegistroDeVendasController.cs
@@ -23,6 +23,16 @@ namespace VendaWebMvc.Controllers
 
         public async Task<IActionResult> BuscaSimples( DateTime? dataMin, DateTime? dataMax)
         {
+            if (!dataMin.HasValue)
+            {
+                dataMin = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!dataMax.HasValue)
+            {
+                dataMax = DateTime.Now;
+            }
+            ViewData["dataMin"] = dataMin.Value.ToString("yyyy-MM-dd");
+            ViewData["dataMax"] = dataMax.Value.ToString("yyyy-MM-dd");
             var reseultado = await _registroDeVendaService.BuscaPorDataAsync(dataMin, dataMax);
             return View(reseultado);
         }
